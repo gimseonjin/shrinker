d0b9676ff9af3a9511602968f7b7690f0c1697b3
gimseonjin <kimseonjin616@naver.com>
2022-06-07 11:02:50 +0900

    Feature : Create Index Views
    Create Index Views for testing
    
    참고 : #

--- client/views.py
+++ client/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from django.contrib.auth.models import User
 
 # Create your views here.
+
+
+def index(request):
+    user = User.objects.filter(username = "gimseonjin").first()
+    email = user.email if user else "Anoymouse User!"
+    if request.user.is_authenticated is False:
+        email = "Anonymouse User!"
+    return render(request, "base.html", {"welcome_msg" : f'Hello {email}'})
+
