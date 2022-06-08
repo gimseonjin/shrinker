52d4988417ab1c3f1e20c85894c16e6946d3957d
gimseonjin <kimseonjin616@naver.com>
2022-06-08 09:45:08 +0900

    Build : Add PayPlan and User Detail In Admin.py
    To deal with PayPlan and UserDetail in Admin page, add models in admin.py
    
    참고 : #

--- client/admin.py
+++ client/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from client.models import PayPlan
+from client.models import UserDetail
+
 # Register your models here.
+
+
+admin.site.register(PayPlan)
+admin.site.register(UserDetail)
